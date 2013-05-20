c5884651ac0ad375d4389c96ebaf8b76869c272e
Eric Brundick <spirilis@linux.com>
2013-05-20 10:20:37 -0400

    Changes incorporating Stellaris LaunchPad support.

--- Enrf24.h
+++ Enrf24.h
@@ -33,6 +33,33 @@
 #include <stdint.h>
 #include "nRF24L01.h"
 
+#if defined(__LM4F120H5QR__)
+#include <inc/hw_types.h>
+#include <inc/hw_memmap.h>
+#include <driverlib/sysctl.h>
+#include <driverlib/pin_map.h>
+#include <driverlib/gpio.h>
+#endif
+
+#ifndef BITF
+  #define BIT0                (0x0001)
+  #define BIT1                (0x0002)
+  #define BIT2                (0x0004)
+  #define BIT3                (0x0008)
+  #define BIT4                (0x0010)
+  #define BIT5                (0x0020)
+  #define BIT6                (0x0040)
+  #define BIT7                (0x0080)
+  #define BIT8                (0x0100)
+  #define BIT9                (0x0200)
+  #define BITA                (0x0400)
+  #define BITB                (0x0800)
+  #define BITC                (0x1000)
+  #define BITD                (0x2000)
+  #define BITE                (0x4000)
+  #define BITF                (0x8000)
+#endif
+
 /* Constants for speed, radio state */
 #define ENRF24_STATE_NOTPRESENT 0
 #define ENRF24_STATE_DEEPSLEEP 1
---
+++ examples/AStelTXdemo/AStelTXdemo.ino
@@ -0,0 +1,72 @@
+#include <Enrf24.h>
+#include <nRF24L01.h>
+#include <string.h>
+#include <SPI.h>
+
+Enrf24 radio(PE_1, PE_2, PE_3);  // P2.0=CE, P2.1=CSN, P2.2=IRQ
+const uint8_t txaddr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0x01 };
+
+const char *str_on = "ON";
+const char *str_off = "OFF";
+
+void dump_radio_status_to_serialport(uint8_t);
+
+void setup() {
+  Serial.begin(9600);
+
+  SPI.setModule(3);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+ SPI.setDataMode(SPI_MODE0);
+  SPI.setBitOrder(1);
+
+  radio.begin();  // Defaults 1Mbps, channel 0, max TX power
+  dump_radio_status_to_serialport(radio.radioState());
+
+  radio.setTXaddress((void*)txaddr);
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(str_on);
+  radio.print(str_on);
+  radio.flush();  // Force transmit (don't wait for any more data)
+  dump_radio_status_to_serialport(radio.radioState());  // Should report IDLE
+  delay(1000);
+
+  Serial.print("Sending packet: ");
+  Serial.println(str_off);
+  radio.print(str_off);
+  radio.flush();  //
+  dump_radio_status_to_serialport(radio.radioState());  // Should report IDLE
+  delay(1000);
+}
+
+void dump_radio_status_to_serialport(uint8_t status)
+{
+  Serial.print("Enrf24 radio transceiver status: ");
+  switch (status) {
+    case ENRF24_STATE_NOTPRESENT:
+      Serial.println("NO TRANSCEIVER PRESENT");
+      break;
+
+    case ENRF24_STATE_DEEPSLEEP:
+      Serial.println("DEEP SLEEP <1uA power consumption");
+      break;
+
+    case ENRF24_STATE_IDLE:
+      Serial.println("IDLE module powered up w/ oscillators running");
+      break;
+
+    case ENRF24_STATE_PTX:
+      Serial.println("Actively Transmitting");
+      break;
+
+    case ENRF24_STATE_PRX:
+      Serial.println("Receive Mode");
+      break;
+
+    default:
+      Serial.println("UNKNOWN STATUS CODE");
+  }
+}
